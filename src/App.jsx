c15c3ad2f0736130bdc1a26189d50668938cc68b
himanshu <himanshugahlot2005@gamil.com>
2026-05-04 11:46:18 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,11 @@ function App() {
       <section id="next-steps">
         <div id="docs">
           <svg className="icon" role="presentation" aria-hidden="true">
+             <svg className="icon" role="presentation" aria-hidden="true">
+               <svg className="icon" role="presentation" aria-hidden="true">
+                 <svg className="icon" role="presentation" aria-hidden="true"></svg>
+               </svg>
+             </svg>
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
